f4adb20d8981dc5915dd05654e4b52f922da7c19
salimakhond <salimakhond@gmai.com>
2023-03-27 17:38:36 +0600

    first commit

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../Header/Header.css'
+import logo from '../../images/Logo.svg'
+
+const Header = () => {
+    return (
+        <nav>
+            <div className='header'>
+                <img src={logo} alt="" />
+                <div>
+                    <a href="order">Order</a>
+                    <a href="orderReview">Order Review</a>
+                    <a href="manageInventory">Manage Inventory</a>
+                    <a href="login">Login</a>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
